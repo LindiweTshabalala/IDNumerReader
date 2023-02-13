a389e8f894283f352bb321f6c2f1ae85613191bf
Lindiwe Tshabalala <lindiwe@bbd.co.za>
2023-02-13 22:51:25 +0200

    added functionality to write to file people born before 2010 and after 2010

--- ReadFromAFile/Program.cs
+++ ReadFromAFile/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Text.RegularExpressions;
 
 namespace IDNumberReader
@@ -12,16 +11,39 @@ namespace IDNumberReader
             
             try 
             {
+                IDReader idReader = new IDReader(filePath);
+                idReader.ProcessIDNumbers();
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+    
+    class IDReader
+    {
+        private string[] idNumbers;
+        private Regex regex = new Regex(@"^\d{13}$");
+
+        public IDReader(string filePath)
+        {
             if (!File.Exists(filePath))
             {
                 throw new FileNotFoundException("File not found: " + filePath);
             }
 
-            string[] idNumbers = File.ReadAllLines(filePath);
-            
-            string pattern = @"^\d{13}$";
-            Regex regex = new Regex(pattern);
+            idNumbers = File.ReadAllLines(filePath);
+        }
 
+        public void ProcessIDNumbers()
+        {
+
+            int before2010Count = 0;
+            int after2010Count = 0;
+            string before2010IDNumbers = "";
+            string after2010IDNumbers = "";
+            
             foreach (string idNumber in idNumbers)
             {
                 if (!regex.IsMatch(idNumber))
@@ -30,26 +52,74 @@ namespace IDNumberReader
                     continue;
                 }
 
-                int yearPrefix = int.Parse(idNumber.Substring(0, 2));
-                int year = yearPrefix < 22 ? 2000 + yearPrefix : 1900 + yearPrefix;
-                int month = int.Parse(idNumber.Substring(2, 2));
-                int day = int.Parse(idNumber.Substring(4, 2));
-
-                if (month < 1 || month > 12 || day < 1 || day > 31)
+                IDInfo idInfo = new IDInfo(idNumber);
+                if (!idInfo.IsValid())
                 {
                     Console.WriteLine("Invalid date of birth: " + idNumber);
                     continue;
                 }
 
-                DateTime dateOfBirth = new DateTime(year, month, day);
-                Console.WriteLine("Date of birth: " + dateOfBirth.ToString("dd/MM/yyyy"));
+                int year = idInfo.GetYear();
+                if (year < 2010) 
+                {
+                    before2010Count++;
+                    before2010IDNumbers += idNumber + Environment.NewLine;
                 }
+                else 
+                {
+                    after2010Count++;
+                    after2010IDNumbers += idNumber + Environment.NewLine;
+                }
+
+                WriteToFile("before2010.txt", before2010Count, before2010IDNumbers);
+                WriteToFile("after2010.txt", after2010Count, after2010IDNumbers);
+
+                Console.WriteLine("Date of birth: " + idInfo.GetDateOfBirth());
             }
-            catch (FileNotFoundException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            }
+        }
+
+        private void WriteToFile(string fileName, int count, string idNumbers)
+        {
+            string fileContent = "Number of people: " + count + Environment.NewLine;
+            fileContent += "ID Numbers:" + Environment.NewLine;
+            fileContent += idNumbers;
+            File.WriteAllText(fileName, fileContent);
         }
     }
 
+    class IDInfo
+    {
+        private int yearPrefix;
+        private int year;
+        private int month;
+        private int day;
+
+        public IDInfo(string idNumber)
+        {
+            yearPrefix = int.Parse(idNumber.Substring(0, 2));
+            year = yearPrefix < 22 ? 2000 + yearPrefix : 1900 + yearPrefix;
+            month = int.Parse(idNumber.Substring(2, 2));
+            day = int.Parse(idNumber.Substring(4, 2));
+        }
+
+        public bool IsValid()
+        {
+            bool isValidMonth = (month >= 1 && month <= 12);
+            bool isValidDay = (day >= 1 && day <= 31);
+            bool isValidDate = (isValidMonth && isValidDay);
+
+            return isValidDate;
+        }
+
+        public string GetDateOfBirth()
+        {
+            DateTime dateOfBirth = new DateTime(year, month, day);
+            return dateOfBirth.ToString("dd/MM/yyyy");
+        }
+
+        public int GetYear()
+        {
+            return year;
+        }
+    }
+}
